377d1d9511964257a1f4214bb50a8a7a8150a58f
Marat <m.brezin@yandex.ru>
2025-09-23 17:38:57 +0300

    add 8 task

--- additional/task_8.cpp
+++ additional/task_8.cpp
@@ -1,3 +1,12 @@
 //
 // Created by Marat on 21.09.25.
 //
+
+#include <iostream>
+
+int main() {
+    int a, b, n;
+    std::cin >> a >> b >> n;
+    int s = (b + n - 1) / n;
+    std::cout << (a > s ? "YES" : "NO");
+}
