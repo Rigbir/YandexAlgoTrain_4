8a5a8a8a3f478c709dcf0d66a9fa2bda42e7a438
Marat <m.brezin@yandex.ru>
2025-09-24 02:14:58 +0300

    add 2 task

--- shortest paths in weighted graphs/task_2.cpp
+++ shortest paths in weighted graphs/task_2.cpp
@@ -1,3 +1,60 @@
 //
 // Created by Marat on 23.09.25.
 //
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n, s, f;
+    std::cin >> n >> s >> f;
+
+    std::vector<std::vector<int>> matrix(n + 1, std::vector<int>(n + 1));
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    std::vector<int> prev(n + 1, -1);
+    std::vector<bool> visited(n + 1, false);
+
+    std::vector<int> dist(n + 1, 1e9);
+    dist[s] = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        int nearest = -1;
+        for (int v = 1; v <= n; ++v) {
+            if (!visited[v] && (nearest == -1 || dist[nearest] > dist[v])) {
+                nearest = v;
+            }
+        }
+
+        if (dist[nearest] == 1e9) break;
+        visited[nearest] = true;
+
+        for (int to = 1; to <= n; ++to) {
+            if (matrix[nearest][to] != -1) {
+                if (dist[to] > dist[nearest] + matrix[nearest][to]) {
+                    dist[to] = dist[nearest] + matrix[nearest][to];
+                    prev[to] = nearest;
+                }
+            }
+        }
+    }
+
+    if (dist[f] == 1e9) {
+        std::cout << -1;
+    } else {
+        std::vector<int> path;
+        for (int v = f; v != -1; v = prev[v]) {
+            path.push_back(v);
+        }
+        std::ranges::reverse(path);
+
+        for (auto i : path) {
+            std::cout << i << ' ';
+        }
+    }
+}
