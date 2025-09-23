6ec557f02990b11f26eb38bd092c82fc275b7391
Marat <m.brezin@yandex.ru>
2025-09-23 17:48:29 +0300

    add 9 task

--- additional/task_9.cpp
+++ additional/task_9.cpp
@@ -1,3 +1,38 @@
 //
 // Created by Marat on 21.09.25.
 //
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+int main() {
+    std::string inputLine;
+    std::cin >> inputLine;
+
+    bool ok = true;
+    std::stack<char> st;
+    for (char c : inputLine) {
+        if (c == '(' || c == '[' || c == '{') {
+            st.push(c);
+        } else {
+            if (st.empty()) {
+                ok = false;
+                break;
+            }
+
+            if ((c == ')' && st.top() == '(') ||
+                (c == ']' && st.top() == '[') ||
+                (c == '}' && st.top() == '{')) {
+                st.pop();
+            } else {
+                ok = false;
+                break;
+            }
+        }
+    }
+
+    if (!st.empty()) ok = false;
+
+    std::cout << (ok ? "yes" : "no");
+}
