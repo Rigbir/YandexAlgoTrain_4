272816f47eb5a4a9e1433952357003ebd71f8619
Marat <m.brezin@yandex.ru>
2025-09-23 17:59:15 +0300

    add 10 task

--- additional/task_10.cpp
+++ additional/task_10.cpp
@@ -1,3 +1,24 @@
 //
 // Created by Marat on 21.09.25.
 //
+
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        int n, a, b;
+        std::cin >> n >> a >> b;
+
+        int minGroups = (n + b - 1) / b;
+        int maxGroups = n / a;
+
+        if (minGroups <= maxGroups) {
+            std::cout << "YES\n";
+        } else {
+            std::cout << "NO\n";
+        }
+    }
+}
